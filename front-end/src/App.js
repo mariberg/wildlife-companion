ddb15ada6182ade6a8ce0714924b20849413dbdb
MARYANNE67 <93769788+MARYANNE67@users.noreply.github.com>
2023-08-19 17:55:30 -0400

    Rearranged component paths for better understanding v2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import ChatBot from './components/pages/chatbot/ChatBot';
-import NavBar from './components/pages/navbar/NavBar';
+import ChatBot from './components/chatbot/ChatBot';
+import NavBar from './components/navbar/NavBar';
 import BirdMap from './components/pages/map/Map';
 import Home from './components/pages/home/Home';
 import Profile from './components/pages/profile/Profile';
