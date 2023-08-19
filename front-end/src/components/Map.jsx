1a9fba9b2873301cb28de751dc1a7928acc50b62
MARYANNE67 <93769788+MARYANNE67@users.noreply.github.com>
2023-08-18 20:25:18 -0400

    Map Demo with hardcoded geo-location

--- front-end/src/components/Map.jsx
+++ front-end/src/components/Map.jsx
@@ -1,31 +1,168 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import BirdInfoPanel from './BirdInfoPanel';
+import './MapCards.css';
+import { useSpring, animated } from 'react-spring';
 
-const Map = ({ coordinates }) => {
-  const mapRef = useRef(null); // Reference to the map instance
+// import Icon from '@ant-design/icons/lib/components/Icon';
+const status =[
+  'Critically Endangered',
+  'Endangered',
+  'Vulnerable',
+  'Near Threatened',
+  'Least Concern',
+];
 
-  useEffect(() => {
-    if (!mapRef.current) {
-      // Create a map instance if it's not already initialized
-      const map = L.map('map').setView([27.0, 67.0], 8); // Initial center and zoom level
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      }).addTo(map);
-      mapRef.current = map; // Save the map instance in the ref
-    }
+ 
 
-    const circleOptions = { color: 'red', fillOpacity: 1, radius: 5 };
 
-    coordinates.forEach((coord, index) => {
-      L.circleMarker([coord.latitude, coord.longitude], circleOptions)
-        .addTo(mapRef.current) // Use the saved map instance from the ref
-        .bindPopup('Location ' + (index + 1));
-    });
-  }, [coordinates]);
+const BirdMap = () => {
+  const [selectedBird, setSelectedBird] = useState(null);
+  const [birdLong, setBirdLong] = useState([]);
+  const [birdLat, setBirdLat] = useState([]);
 
-  return <div id="map" style={{ width: '100%', height: '500px' }}></div>;
+
+
+
+const birdData = [
+  {
+    id: 1,
+    name: 'Betty',
+    scientificName: 'Vanellus gregarius',
+    conversationStatus: status[0],
+    lat: 48.86,
+    lng: 2.3522,
+    image: 'https://i.pinimg.com/originals/8d/52/50/8d525006da7ce2d9c5e4a4c419806396.jpg',
+    description: 'The sociable lapwing, referred to as the sociable plover in the UK, is a wader in the plover family. It is a fully migratory bird, breeding in Kazakhstan and wintering in the Middle East, Indian Subcontinent, and Sudan. Historical literature referred to this bird as the Black-bellied lapwing.',
+  },
+  {
+    id: 2,
+    name: 'Anna',
+    scientificName: 'Vanellus gregarius',
+    conversationStatus: status[2],
+    lat: 48.85,
+    lng: 2.3522,
+    image: 'https://beachbaby.net/wp-content/uploads/2018/08/bird-black-fronted-tern.jpg',
+    description: 'Description for Bird 2.',
+  },
+  {
+    id: 3,
+    name: 'Kitty',
+    scientificName: 'Vanellus gregarius',
+    conversationStatus: status[0],
+    lat: 48.855,
+    lng: 2.34,
+    image: 'https://keolamagazine.com/wp-content/uploads/2019/08/endangered-birds-3-1024x773.jpg',
+    description: 'Description for Bird 2.',
+  },
+  {
+    id: 4,
+    name: 'Amanda',
+    scientificName: 'Vanellus gregarius',
+    conversationStatus: status[0],
+    lat:48.87,
+    lng: 2.3001,
+    image: 'http://indiasendangered.com/wp-content/uploads/2014/01/The-Endangered-Crimson-backed-Sunbird-was-seen-at-Chandoli-National-Park.jpg',
+    description: 'Description for Bird 2.',
+  },
+  {
+    id: 5,
+    name: 'Jess',
+    scientificName: 'Vanellus gregarius',
+    conversationStatus: status[1],
+    lat:48.97,
+    lng: 2.3001,
+    image: 'https://www.treehugger.com/thmb/-zUa4yqjs3WyyFkIq91qXJUJE-M=/1024x768/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__treehugger__images__2013__08__scrubjay-24a4199239484d52b0274ba8268810bb.jpg',
+    description: 'Description for Bird 2.',
+  },
+  // ... add more bird data
+];
+
+    const [slideProps, setSlide] = useSpring(() => ({
+    transform: 'translateX(0px)',
+  }));
+  
+  const handleMarkerClick = (bird) => {
+    setSelectedBird(bird);
+  };
+
+  // useEffect(() => {
+  //   // Define a function to fetch bird data from the API
+  //   const fetchBirdData = async () => {
+  //     try {
+  //       const response = await axios.get('https://qnildvwtq7.execute-api.eu-west-2.amazonaws.com/dev');
+  //       // Assuming the API response is an array of objects with 'latitude' and 'longitude' properties
+  //       const apiBirdData = response.data;
+
+  //       // Extract latitude and longitude from the API response
+  //       const latitudes = apiBirdData.map(bird => bird.latitude);
+  //       const longitudes = apiBirdData.map(bird => bird.longitude);
+  //         console.log(latitudes);
+  //         console.log(longitudes);
+  //       // Update the state with the fetched data
+  //       setBirdLat(latitudes);
+  //       setBirdLong(longitudes);
+  //     } catch (error) {
+  //       console.error('Error fetching bird data:', error);
+  //     }
+  //   };
+
+  //   // Call the function to fetch bird data when the component mounts
+  //   fetchBirdData();
+  // }, []); 
+
+  return (
+    <div className="bird-map-container">
+      <MapContainer center={[48.8566, 2.3522]} zoom={13} style={{ height: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {birdData.map(bird => (
+          <Marker
+            key={bird.id}
+            position={[bird.lat, bird.lng]}
+            icon={L.divIcon({
+              className: 'bird-marker',
+              html: `<div class="bird-image"><img src="${bird.image}" alt="${bird.name}" /></div>`,
+              iconSize: [35, 35],
+            })}
+            onClick={() => setSelectedBird(bird)}
+          >
+                 <Popup>
+              <div className="popup-content">
+                <h4>{bird.name}</h4>
+                <p>{bird.scientificName}</p>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+     
+      <div className="bird-cards">
+        <animated.div className="cards-slider" style={slideProps}>
+          {birdData.map((bird) => (
+            <div
+              className={`bird-polaroid ${selectedBird === bird ? 'active' : ''}`}
+              key={bird.id}
+              onClick={() => handleMarkerClick(bird)}
+            >
+              <img src={bird.image} alt={bird.name} style={{ width: '100%' }} />
+              <div className="bird-card">
+                <h3>{bird.name}</h3>
+                <p style={{color:'#13B156', fontSize:'0.7rem'}}>Learn more about me</p>
+              </div>
+            </div>
+          ))}
+        </animated.div>
+      </div>
+     
+      <BirdInfoPanel bird={selectedBird} onClose={() => setSelectedBird(null)} />
+    </div>
+  );
 };
 
-export default Map;
+export default BirdMap;
--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {HomeOutlined, GlobalOutlined, BellOutlined, UserOutlined, PoweroffOutlined} from '@ant-design/icons'
+import {Menu, } from 'antd'
+import { useNavigate, useLocation } from 'react-router-dom';
+import './NavBar.css'
+
+// import { Container, Row, Col, Button } from 'react-bootstrap';
+
 
 const NavBar = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    
     return (
-        <nav className="navbar">
-            <ul className="nav-list">
-                <li className="nav-item">
-                    <NavLink exact to="/" activeClassName="active">Home</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to="/map" activeClassName="active">Map</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to="/chatbot" activeClassName="active">ChatBot</NavLink>
-                </li>
-            </ul>
-        </nav>
-    );
+      <>
+        <div className="navbar-container" style={{display:'flex', flexDirection:'column', fontWeight:'30px', fontSize:'20px'}}>
+        <div className="navbar-header">
+        <span  style={{ whiteSpace: 'nowrap' }}><b>Wildlife Companion</b></span>
+      </div>
+      <Menu 
+      selectedKeys={[location.pathname]}
+      onClick={({key }) => {
+            if(key === 'signout'){}
+            else{
+                navigate(key);
+            }
+      }}>
+
+      <Menu.Item key="/" icon={<HomeOutlined />} className={location.pathname === '/' ? 'active-menu-item' : ''}>
+          Home
+        </Menu.Item>
+        <Menu.Item key="/map" icon={<GlobalOutlined />} className={location.pathname === '/map' ? 'active-menu-item' : ''}>
+          Explore
+        </Menu.Item>
+        <Menu.Item key="/chatbot" icon={<BellOutlined />} className={location.pathname === '/chatbot' ? 'active-menu-item' : ''}>
+          Notifications
+        </Menu.Item>
+        <Menu.Item key="/profile" icon={<UserOutlined />} className={location.pathname === '/profile' ? 'active-menu-item' : ''}>
+          Profile
+        </Menu.Item>
+        <Menu.Item key="signout" icon={<PoweroffOutlined />} danger>
+          Logout
+        </Menu.Item>
+        </Menu>  
+    </div>
+    </>
+    )
+    
+
 };
 
+
+
+
 export default NavBar;
+
