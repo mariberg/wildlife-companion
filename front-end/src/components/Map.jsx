9245203dbfbf71b355bb8d0187bb19f1950880c4
MARYANNE67 <93769788+MARYANNE67@users.noreply.github.com>
2023-08-19 14:37:56 -0400

    Profile page update

--- front-end/src/components/Map.jsx
+++ front-end/src/components/Map.jsx
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -86,6 +86,20 @@ const BirdMap = () => {
     setSelectedBird(bird);
   };
 
+
+  useEffect(() => {
+    setSlide({ transform: `translateX(0px)` });
+  }, [selectedBird]);
+
+  const handleSlide = (direction) => {
+    const cardWidth = 240; // Adjust this width as needed
+    const totalCards = birdData.length;
+    const maxWidth = cardWidth * (totalCards - 1);
+    let newTranslateX = slideProps.transform[0] + direction * cardWidth;
+    newTranslateX = Math.max(-maxWidth, Math.min(0, newTranslateX));
+    setSlide({ transform: `translateX(${newTranslateX}px)` });
+  };
+
   // useEffect(() => {
   //   // Define a function to fetch bird data from the API
   //   const fetchBirdData = async () => {
@@ -140,7 +154,8 @@ const BirdMap = () => {
       </MapContainer>
      
       <div className="bird-cards">
-        <animated.div className="cards-slider" style={slideProps}>
+      <div className="slider-container">
+        <animated.div className="cards-slider" style={{ transform: slideProps.transform }}>
           {birdData.map((bird) => (
             <div
               className={`bird-polaroid ${selectedBird === bird ? 'active' : ''}`}
@@ -156,10 +171,84 @@ const BirdMap = () => {
           ))}
         </animated.div>
       </div>
-     
+      <div className="slider-controls">
+           <button onClick={() => handleSlide(1)}>&lt;</button>
+           <button onClick={() => handleSlide(-1)}>&gt;</button>
+         </div>
+       </div>
+
       <BirdInfoPanel bird={selectedBird} onClose={() => setSelectedBird(null)} />
     </div>
   );
 };
 
 export default BirdMap;
+
+
+// const BirdMap = () => {
+//   const [selectedBird, setSelectedBird] = useState(null);
+//   const [birdLong, setBirdLong] = useState([]);
+//   const [birdLat, setBirdLat] = useState([]);
+
+
+
+//   return (
+//     <div className="bird-map-container">
+//       <MapContainer center={[48.8566, 2.3522]} zoom={13} style={{ height: '100%' }}>
+    
+//          <TileLayer
+//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         />
+//         {birdData.map(bird => (
+//           <Marker
+//             key={bird.id}
+//             position={[bird.lat, bird.lng]}
+//             icon={L.divIcon({
+//               className: 'bird-marker',
+//               html: `<div class="bird-image"><img src="${bird.image}" alt="${bird.name}" /></div>`,
+//               iconSize: [35, 35],
+//             })}
+//             onClick={() => setSelectedBird(bird)}
+//           >
+//                  <Popup>
+//               <div className="popup-content">
+//                 <h4>{bird.name}</h4>
+//                 <p>{bird.scientificName}</p>
+//               </div>
+//             </Popup>
+//           </Marker>
+//         ))}
+
+//       </MapContainer>
+
+//       <div className="bird-cards">
+//         <div className="slider-container">
+//           <animated.div className="cards-slider" style={{ transform: slideProps.transform }}>
+//             {birdData.map((bird) => (
+//               <div
+//                 className={`bird-polaroid ${selectedBird === bird ? 'active' : ''}`}
+//                 key={bird.id}
+//                 onClick={() => handleMarkerClick(bird)}
+//               >
+//                 <img src={bird.image} alt={bird.name} style={{ width: '100%' }} />
+//                 <div className="bird-card">
+//                   <h3>{bird.name}</h3>
+//                   <p style={{ color: '#13B156', fontSize: '0.7rem' }}>Learn more about me</p>
+//                 </div>
+//               </div>
+//             ))}
+//           </animated.div>
+//         </div>
+//         <div className="slider-controls">
+//           <button onClick={() => handleSlide(1)}>&lt;</button>
+//           <button onClick={() => handleSlide(-1)}>&gt;</button>
+//         </div>
+//       </div>
+
+//       <BirdInfoPanel bird={selectedBird} onClose={() => setSelectedBird(null)} />
+//     </div>
+//   );
+// };
+
+// export default BirdMap;
