1f259dffbfb6d02094a30cc006e42a6ca66608e7
Marika <marika.bergman@gmail.com>
2023-08-21 18:46:40 +0100

    fix map component to use coordinates from api call

--- front-end/src/components/pages/map/Map.jsx
+++ front-end/src/components/pages/map/Map.jsx
@@ -1,76 +1,91 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Circle, Popup, Marker } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import BirdInfoPanel from './BirdInfoPanel';
 import './MapCards.css';
 import { useSpring, animated } from 'react-spring';
-import  {birdData}  from '../../data/BirdData';
+import { birdData } from '../../data/BirdData';
 
-// import Icon from '@ant-design/icons/lib/components/Icon';
- 
 const BirdMap = () => {
   const [selectedBird, setSelectedBird] = useState(null);
-  const [birdLong, setBirdLong] = useState([]);
-  const [birdLat, setBirdLat] = useState([]);
+  const [birdCoordinates, setBirdCoordinates] = useState([]);
 
-
-    const [slideProps, setSlide] = useSpring(() => ({
+  const [slideProps, setSlide] = useSpring(() => ({
     transform: 'translateX(0px)',
   }));
-  
+
   const handleMarkerClick = (bird) => {
     setSelectedBird(bird);
   };
 
-
   useEffect(() => {
-    setSlide({ transform: `translateX(0px)` });
+    setSlide({ transform: 'translateX(0px)' });
   }, [selectedBird]);
 
   const handleSlide = (direction) => {
     const cardWidth = 240; // Adjust this width as needed
-    const totalCards = birdData.length;
+    const totalCards = birdCoordinates.length;
     const maxWidth = cardWidth * (totalCards - 1);
     let newTranslateX = slideProps.transform[0] + direction * cardWidth;
     newTranslateX = Math.max(-maxWidth, Math.min(0, newTranslateX));
     setSlide({ transform: `translateX(${newTranslateX}px)` });
   };
 
-    // TODO: ERROR: CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+  useEffect(() => {
+    axios.get("https://6wu36vx0al.execute-api.eu-west-2.amazonaws.com")
+      .then(response => {
+        const data = response.data;
+        setBirdCoordinates(data);
+      })
+      .catch(error => console.log(error));
+  }, []);
 
+  // Calculate the center point of the displayed coordinates
+  const centerPoint = birdCoordinates.reduce(
+    (acc, bird) => ({
+      lat: acc.lat + bird.latitude,
+      lng: acc.lng + bird.longitude,
+    }),
+    { lat: 0, lng: 0 }
+  );
 
+  // Check if birdCoordinates are valid and not empty
+  if (birdCoordinates.length === 0) {
+    console.log("No coordinates fetched");
+    return null; // Or display loading message
+  }
 
-
-
-      useEffect(() => {
-        // Define a function to fetch bird data from the API
-       fetch("https://6wu36vx0al.execute-api.eu-west-2.amazonaws.com")
-       .then(response => response.json())
-       .then(data => 
-          {
-            console.log(data);
-            setBirdLong(data.map(bird => bird.longitude));
-            setBirdLat(data.map(bird => bird.latitude));
-          }
-       )
-       .catch(error => console.log(error));
-    
-        // Call the function to fetch bird data when the component mounts
-       
-      }, []); 
-     
-
-    
+  // Calculate the average center
+  const center = [centerPoint.lat / birdCoordinates.length, centerPoint.lng / birdCoordinates.length];
 
   return (
     <div className="bird-map-container">
-      <MapContainer center={[48.8566, 2.3522]} zoom={13} style={{ height: '100%' }}>
+      <MapContainer center={center} zoom={8} style={{ height: '100%' }}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {birdCoordinates.map((bird, index) => (
+          <Circle
+            key={index}
+            center={[bird.latitude, bird.longitude]}
+            pathOptions={{
+              color: 'red',
+              fillColor: index === birdCoordinates.length - 1 ? '#f00' : '#f03',
+              fillOpacity: 0.5,
+            }}
+            radius={index === birdCoordinates.length - 1 ? 1500 : 500}
+          >
+            <Popup>
+              <div className="popup-content">
+                <h4>Location {index + 1}</h4>
+                {/* Add more information as needed */}
+              </div>
+            </Popup>
+          </Circle>
+        ))}
         {birdData.map(bird => (
           <Marker
             key={bird.id}
@@ -80,9 +95,9 @@ const BirdMap = () => {
               html: `<div class="bird-image"><img src="${bird.image}" alt="${bird.name}" /></div>`,
               iconSize: [35, 35],
             })}
-            onClick={() => setSelectedBird(bird)}
+            onClick={() => handleMarkerClick(bird)}
           >
-                 <Popup>
+            <Popup>
               <div className="popup-content">
                 <h4>{bird.name}</h4>
                 <p>{bird.scientificName}</p>
@@ -91,34 +106,36 @@ const BirdMap = () => {
           </Marker>
         ))}
       </MapContainer>
-     
       <div className="bird-cards">
-      <div className="slider-container">
-        <animated.div className="cards-slider" style={{ transform: slideProps.transform }}>
-          {birdData.map((bird) => (
-            <div
-              className={`bird-polaroid ${selectedBird === bird ? 'active' : ''}`}
-              key={bird.id}
-              onClick={() => handleMarkerClick(bird)}
-            >
-              <img src={bird.image} alt={bird.name} style={{ width: '100%' }} />
-              <div className="bird-card">
-                <h3>{bird.name}</h3>
-                <p style={{color:'#13B156', fontSize:'0.7rem'}}>Learn more about me</p>
+        <div className="slider-container">
+          <animated.div className="cards-slider" style={{ transform: slideProps.transform }}>
+            {birdData.map((bird) => (
+              <div
+                className={`bird-polaroid ${selectedBird === bird ? 'active' : ''}`}
+                key={bird.id}
+                onClick={() => handleMarkerClick(bird)}
+              >
+                <img src={bird.image} alt={bird.name} style={{ width: '100%' }} />
+                <div className="bird-card">
+                  <h3>{bird.name}</h3>
+                  <p style={{ color: '#13B156', fontSize: '0.7rem' }}>Learn more about me</p>
+                </div>
               </div>
-            </div>
-          ))}
-        </animated.div>
+            ))}
+          </animated.div>
+        </div>
+        <div className="slider-controls">
+          <button onClick={() => handleSlide(1)}>&lt;</button>
+          <button onClick={() => handleSlide(-1)}>&gt;</button>
+        </div>
       </div>
-      <div className="slider-controls">
-           <button onClick={() => handleSlide(1)}>&lt;</button>
-           <button onClick={() => handleSlide(-1)}>&gt;</button>
-         </div>
-       </div>
-
       <BirdInfoPanel bird={selectedBird} onClose={() => setSelectedBird(null)} />
     </div>
   );
 };
 
 export default BirdMap;
+
+
+
+
